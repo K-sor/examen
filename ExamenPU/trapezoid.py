51b6e2038dc857e7a439ec45c002262d0c3d458d
K-sor <149714531+K-sor@users.noreply.github.com>
2025-03-08 16:43:03 -0600

    Add files via upload

---
+++ ExamenPU/trapezoid.py
@@ -0,0 +1,7 @@
+# Función para calcular el área de un trapecio
+def calcular_area(base_mayor, base_menor, altura):
+    return (base_mayor + base_menor) * altura / 2
+
+# Función para calcular el perímetro de un trapecio
+def calcular_perimetro(base_mayor, base_menor, lado_1, lado_2):
+    return base_mayor + base_menor + lado_1 + lado_2
